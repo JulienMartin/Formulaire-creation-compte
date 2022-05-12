d5a14ae96921178a9efdc8c2b30f5f875f22c531
JulienMartin <julien.martin2@yahoo.fr>
2022-05-12 16:12:55 +0200

    update-icones-messages

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ This repo shows a form the user has to fill in order to register on a specific w
 
 A check is made on every item of the form.
 
+Technologies used :
+
+- HTML
+- CSS + Bootstrap
+- Javascript
+- Import Forkawesome
+
 The project is still ongoing, some updates and improvements will be made from time to time.
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-// Test en console
-console.log('Hello tout le monde');
-
 
 // On définit nos variables en ciblant les 'id' du document
 let form = document.getElementById('form');
@@ -28,30 +25,30 @@ function checkForm(){
 
         // Si le pseudo est vide, on affiche un message
         if(pseudoValue == ''){
-            errorCheck(pseudo, 'Le pseudo ne peut pas être vide');
+            errorCheck(pseudo, ' Le pseudo ne peut pas être vide');
         } else {
             successCheck(pseudo);
         }
 
         // Si l'e-mail est vide, on affiche un message
         if(emailValue == ''){
-            errorCheck(email, 'L\'e-mail ne peut pas être vide');
+            errorCheck(email, ' L\'e-mail ne peut pas être vide');
         } else {
             successCheck(email);
         }
 
         // Si le mot de passe est vide, on affiche un message
         if(passwordValue == ''){
-            errorCheck(password, 'Le mot de passe ne peut pas être vide');
+            errorCheck(password, ' Le mot de passe ne peut pas être vide');
         } else {
             successCheck(password);
         }
 
         // Si le mot de passe est vide, on affiche un message
         if(passwordConfirmValue == ''){
-            errorCheck(passwordConfirm, 'Vous devez confirmer votre mot de passe');
+            errorCheck(passwordConfirm, ' Vous devez confirmer votre mot de passe');
         } else if(passwordConfirmValue != passwordValue) {
-            errorCheck(passwordConfirm, 'Les deux mots de passe ne correspondent pas');
+            errorCheck(passwordConfirm, ' Les deux mots de passe ne correspondent pas');
         } else {
             successCheck(passwordConfirm);
         }
@@ -63,20 +60,35 @@ function errorCheck(errorInput, errorMessage){
     // On cible l'élément parent de chaque entrée du formulaire, à savoir le 'form-control'
     let formCheckElement = errorInput.parentElement;
 
-    // On cible le message d'erreur
-    let errorDiv = formCheckElement.querySelector('.errormsg');
+    // On va ajouter à cette <div> une icone adaptée et un message d'erreur
+    let newMessage = document.createElement('p');
+    newMessage.setAttribute('class', 'errorpar')
+    newMessage.textContent = errorMessage;
+    
+    let newI = document.createElement('i');
+    newI.setAttribute('class', 'fa fa-exclamation-circle');
 
-    // On lui attribue la classe 'error' de façon à afficher le message d'erreur
+    formCheckElement.append(newMessage);
+    newMessage.prepend(newI);
+
+    // On lui attribue la classe 'error' de façon à afficher le logo correspondant et le message d'erreur
     formCheckElement.className = 'form-control error';
-    errorDiv.textContent = errorMessage;
-
 }
 
-// La fonction successCheck va gérer les cas où les formulaires sont correctement remplis
+// La fonction successCheck va gérer le cas des champs de formulaires correctement remplis
 function successCheck(successInput){
 
     // On cible à nouveau l'élément parent de chaque entrée du formulaire
     let formCheckElement = successInput.parentElement;
+
+    // On va ajouter à cette <div> une icone adaptée dans un paragraphe sans texte
+    let newMessage = document.createElement('p');
+    
+    let newI = document.createElement('i');
+    newI.setAttribute('class', 'fa fa-check-circle-o');
+
+    formCheckElement.append(newMessage);
+    newMessage.prepend(newI);
 
     // On lui attribue la classe 'success' de façon à afficher le logo correspondant
     formCheckElement.className = 'form-control success';
